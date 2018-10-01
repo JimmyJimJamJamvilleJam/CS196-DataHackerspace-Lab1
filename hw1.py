8d1a4af42e7a0cd2cd8c796e1934217a5f83666c
JimmyJimJamJamvilleJam <thr2@illinois.edu>
2018-10-01 18:19:44 -0500

    troubles with single_type_candy_count as some parts of the json file lack candycounts

--- hw1.py
+++ hw1.py
@@ -19,8 +19,17 @@ def histogram_times(filename):
         if airplane_data[x][1] != '':
             times.append(airplane_data[x][1])
 
+    strTime = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17',
+               '18', '19', '20', '21', '22', '23']
 
-    return ''
+    timesArray = [0 for i in range(23)]
+    print(timesArray)
+    for time in range(23):
+        for x in range(len(times)):
+            num = times[x][:2]
+            if (num == strTime[time]):
+                timesArray[time] = timesArray[time] + 1
+    return timesArray
 
 
 def weigh_pokemons(filename, weight):
@@ -28,7 +37,7 @@ def weigh_pokemons(filename, weight):
         pokemon_data = json.load(f)
     pokemons = []
     for pokemon in pokemon_data['pokemon']:
-        if pokemon['weight'] == str(float(weight)) +  " kg" :
+        if pokemon['weight'] == str(float(weight)) + " kg":
             pokemons.append(pokemon['name'])
     return pokemons
 
@@ -37,12 +46,12 @@ def single_type_candy_count(filename):
     with open(filename) as f:
         pokemon_data = json.load(f)
     count = 0
-    #print(pokemon_data['candy_count'])
-    for pokemon in pokemon_data['pokemon']:
-        #print(pokemon['candy_count'])
-        #if pokemon['candy_count'] != '':
-         #   count += pokemon['candy_count']
-        print(count)
+    print(pokemon_data)
+    #for pokemon in pokemon_data['pokemon']:
+        # print(pokemon['candy_count'])
+        # if pokemon['candy_count'] != '':
+        #   count += pokemon['candy_count']
+        #print(count)
     return count
 
 
@@ -55,11 +64,11 @@ def reflections_and_projections(points):
     x = np.pi / 2
     rotate_matrix = np.array([[0, -1], [1, 0]])
 
-    arr = np.dot(rotate_matrix,arr)
+    arr = np.dot(rotate_matrix, arr)
 
-    linear_matrix = np.array([[1/10, 3/10], [3/10,9/10]])
+    linear_matrix = np.array([[1 / 10, 3 / 10], [3 / 10, 9 / 10]])
 
-    arr = np.dot(linear_matrix,arr)
+    arr = np.dot(linear_matrix, arr)
 
     return arr
 
@@ -87,9 +96,11 @@ def sigmoid_normalize(image):
 
 
 def main():
-    #histogram_times('airplane_crashes.csv')
-    #single_type_candy_count('pokedex.json')
-    #arr_2d = np.array([np.arange(2 * i, 10 * i + 5) for i in range(10)])
-    #reflections_and_projections([[1,2,3],[4,5,6]])
-    print(normalize([[1,2,3],[4,5,6]]))
+    #print(histogram_times('airplane_crashes.csv'))
+     single_type_candy_count('pokedex.json')
+    # arr_2d = np.array([np.arange(2 * i, 10 * i + 5) for i in range(10)])
+    # reflections_and_projections([[1,2,3],[4,5,6]])
+    # print(normalize([[1,2,3],[4,5,6]]))
+
+
 main()
